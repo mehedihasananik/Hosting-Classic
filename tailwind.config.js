ca1d3b7834131be0310f339139fc1e500c9315d5
marshallanique <marshallanique@gmail.com>
2023-08-31 16:40:50 +0600

    project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
@@ -15,6 +16,6 @@ export default {
     },
   },
   plugins: [
-
+    require('flowbite/plugin')
   ]
 }
